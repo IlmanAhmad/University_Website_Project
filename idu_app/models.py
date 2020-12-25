5af5b2b67b35446856e70945ccb8e6c8d1155d89
Ilman <pythonilmantest@gmail.com>
2020-12-25 17:27:37 +0530

    Added the functionality for bulk update student details and their marksheet data

--- idu_app/models.py
+++ idu_app/models.py
@@ -8,7 +8,7 @@ class student(models.Model):
     stu_first_name = models.CharField(max_length=255, default="")
     stu_last_name = models.CharField(max_length=255, default="")
     stu_full_name = models.CharField(max_length=255, default="")
-    stu_class = models.CharField(max_length=10, default="")
+    stu_semester = models.CharField(max_length=10, default="")
     stu_father_name = models.CharField(max_length=255, default="")
     stu_mother_name = models.CharField(max_length=255, default="")
     stu_contact = models.CharField(max_length=15)
@@ -24,18 +24,18 @@ class mark(models.Model):
     """Class for storing student Marks"""
 
     stu_full_name = models.CharField(max_length=255, default="")
-    stu_roll_no = models.CharField(max_length=255, unique=True, default="")
-    stu_class = models.CharField(max_length=10, default="")
+    stu_roll_no = models.IntegerField(unique=True)
+    stu_semester = models.CharField(max_length=10, default="")
     stu_sub1 = models.CharField(max_length=4, default=0)
     stu_sub2 = models.CharField(max_length=4, default=0)
     stu_sub3 = models.CharField(max_length=4, default=0)
     stu_sub4 = models.CharField(max_length=4, default=0)
     stu_sub5 = models.CharField(max_length=4, default=0)
-    stu_total_marks = models.CharField(max_length=4, default=0)
-    stu_average_marks = models.CharField(max_length=4, default=0)
+    stu_total_marks = models.IntegerField()
+    stu_average_marks = models.IntegerField()
 
     def __str__(self):
-        return self.stu_roll_no
+        return self.stu_full_name
 
 class contact(models.Model):
     """Class for storing contact details requests"""
--- idu_app/views.py
+++ idu_app/views.py
@@ -12,6 +12,7 @@ def home(request):
 
     return render(request, 'idu_app/home.html')
 
+
 def handlelogin(request):
     """function to handle login authentication"""
     if request.method == "POST":
@@ -35,7 +36,6 @@ def handlelogout(request):
     logout(request)
     messages.success(request, "Your have successfully logged out")
     return redirect("idu:home")
-
 
 
 def contactus(request):
@@ -65,10 +65,11 @@ def studetailsadmin(request):
 
     return render(request, 'idu_app/studetailsadmin.html')
 
+
 def sturegisterdetails(request):
     """function to redirect to student details page"""
     if request.method == "POST":
-        stu_roll_no = request.POST.get('roll','')
+        stu_roll_no = request.POST.get('roll', '')
         stu_details = student.objects.filter(stu_roll_no=stu_roll_no).values()
         params = {'stu_details': stu_details}
         return render(request, 'idu_app/sturegisterdetails.html', params)
@@ -79,7 +80,7 @@ def sturegisterdetails(request):
 def results(request):
     """function to redirect to student results page"""
     if request.method == "POST":
-        stu_roll_no = request.POST.get('roll_no','')
+        stu_roll_no = request.POST.get('roll_no', '')
         mark_list = mark.objects.filter(stu_roll_no=stu_roll_no).values()
         params = {'mark_list': mark_list}
         return render(request, 'idu_app/results.html', params)
@@ -89,6 +90,7 @@ def results(request):
 
 def registeration(request):
     """This function is for uploading student details in database"""
+
     if request.method == "POST":
 
         stu_data = pd.read_excel(request.FILES.get(
@@ -102,7 +104,7 @@ def registeration(request):
             stu_first_name = str(stu_data.at[i, 'stu_first_name'])
             stu_last_name = str(stu_data.at[i, 'stu_last_name'])
             stu_full_name = str(stu_data.at[i, 'stu_full_name'])
-            stu_class = str(stu_data.at[i, 'stu_class'])
+            stu_semester = str(stu_data.at[i, 'stu_semester'])
             stu_roll_no = int(stu_data.at[i, 'stu_roll_no'])
             stu_father_name = str(stu_data.at[i, 'stu_father_name'])
             stu_contact = str(stu_data.at[i, 'stu_contact'])
@@ -112,10 +114,11 @@ def registeration(request):
             stu_city = str(stu_data.at[i, 'stu_city'])
             stu_postalcode = str(stu_data.at[i, 'stu_postalcode'])
             students = student(stu_first_name=stu_first_name, stu_last_name=stu_last_name, stu_full_name=stu_full_name,
-                               stu_class=stu_class, stu_roll_no=stu_roll_no, stu_father_name=stu_father_name, stu_contact=stu_contact,
+                               stu_semester=stu_semester, stu_roll_no=stu_roll_no, stu_father_name=stu_father_name, stu_contact=stu_contact,
                                stu_mother_name=stu_mother_name, stu_address_line1=stu_address_line1, stu_address_line2=stu_address_line2,
                                stu_city=stu_city, stu_postalcode=stu_postalcode)
             students.save()
+
         messages.success(
             request, "Your student registeration data has been successfully uploaded.")
         return redirect("idu:studetailsadmin")
@@ -123,8 +126,43 @@ def registeration(request):
         return HttpResponse('404 - Not found')
 
 
+def updatestudetailsadmin(request):
+    """This function is for modifying student details in database"""
+
+    if request.method == "POST":
+        stu_data = pd.read_excel(request.FILES.get(
+            'excelfile'))
+        data_length = range(len(stu_data))
+        for i in data_length:
+            stu_roll_no = int(stu_data.at[i, 'stu_roll_no'])
+            stu_obj = student.objects.get(stu_roll_no=stu_roll_no)
+            if stu_obj.stu_roll_no == stu_roll_no:
+                stu_obj.stu_first_name = str(stu_data.at[i, 'stu_first_name'])
+                stu_obj.stu_last_name = str(stu_data.at[i, 'stu_last_name'])
+                stu_obj.stu_full_name = str(stu_data.at[i, 'stu_full_name'])
+                stu_obj.stu_semester = str(stu_data.at[i, 'stu_semester'])
+                stu_obj.stu_father_name = str(
+                    stu_data.at[i, 'stu_father_name'])
+                stu_obj.stu_contact = str(stu_data.at[i, 'stu_contact'])
+                stu_obj.stu_mother_name = str(
+                    stu_data.at[i, 'stu_mother_name'])
+                stu_obj.stu_address_line1 = str(
+                    stu_data.at[i, 'stu_address_line1'])
+                stu_obj.stu_address_line2 = str(
+                    stu_data.at[i, 'stu_address_line2'])
+                stu_obj.stu_city = str(stu_data.at[i, 'stu_city'])
+                stu_obj.stu_postalcode = str(stu_data.at[i, 'stu_postalcode'])
+
+                stu_obj.save()
+        messages.success(
+            request, "Your student registeration data has been successfully modified.")
+
+        return redirect("idu:studetailsadmin")
+
+
 def marksheet(request):
     """This function is for uploading student marksheet in database"""
+
     if request.method == "POST":
 
         stu_marks = pd.read_excel(request.FILES.get(
@@ -137,7 +175,7 @@ def marksheet(request):
 
             stu_full_name = str(stu_marks.at[i, 'stu_full_name'])
             stu_roll_no = int(stu_marks.at[i, 'stu_roll_no'])
-            stu_class = str(stu_marks.at[i, 'stu_class'])
+            stu_semester = str(stu_marks.at[i, 'stu_semester'])
             stu_sub1 = int(stu_marks.at[i, 'stu_sub1'])
             stu_sub2 = int(stu_marks.at[i, 'stu_sub2'])
             stu_sub3 = int(stu_marks.at[i, 'stu_sub3'])
@@ -147,7 +185,7 @@ def marksheet(request):
                                   stu_sub3 + stu_sub4 + stu_sub5)
             stu_average_marks = int((stu_total_marks/500) * 100)
 
-            marks = mark(stu_full_name=stu_full_name, stu_roll_no=stu_roll_no, stu_class=stu_class,
+            marks = mark(stu_full_name=stu_full_name, stu_roll_no=stu_roll_no, stu_semester=stu_semester,
                          stu_sub1=stu_sub1, stu_sub2=stu_sub2, stu_sub3=stu_sub3, stu_sub4=stu_sub4, stu_sub5=stu_sub5,
                          stu_total_marks=stu_total_marks, stu_average_marks=stu_average_marks)
             marks.save()
@@ -155,3 +193,34 @@ def marksheet(request):
         return redirect("idu:studetailsadmin")
     else:
         return HttpResponse('404 - Not found')
+
+
+def updatemarksheet(request):
+    """This function is for modifying student marksheet data in database"""
+
+    if request.method == "POST":
+        stu_data = pd.read_excel(request.FILES.get(
+            'excelmarksfile'))
+        data_length = range(len(stu_data))
+        for i in data_length:
+            stu_roll_no = int(stu_data.at[i, 'stu_roll_no'])
+            stu_obj = mark.objects.get(stu_roll_no=stu_roll_no)
+            if stu_obj.stu_roll_no == stu_roll_no:
+                stu_obj.stu_full_name = str(stu_data.at[i, 'stu_full_name'])
+                stu_obj.stu_semester = str(stu_data.at[i, 'stu_semester'])
+                stu_obj.stu_sub1 = int(stu_data.at[i, 'stu_sub1'])
+                stu_obj.stu_sub2 = int(stu_data.at[i, 'stu_sub2'])
+                stu_obj.stu_sub3 = int(stu_data.at[i, 'stu_sub3'])
+                stu_obj.stu_sub4 = int(stu_data.at[i, 'stu_sub4'])
+                stu_obj.stu_sub5 = int(stu_data.at[i, 'stu_sub5'])
+                stu_obj.stu_total_marks = int(stu_obj.stu_sub1 + stu_obj.stu_sub2 +
+                                  stu_obj.stu_sub3 + stu_obj.stu_sub4 + stu_obj.stu_sub5)
+                stu_obj.stu_average_marks = int((stu_obj.stu_total_marks/500) * 100)
+
+                stu_obj.save()
+        messages.success(
+            request, "Your student marksheet data has been successfully modified.")
+
+        return redirect("idu:studetailsadmin")
+
+    
